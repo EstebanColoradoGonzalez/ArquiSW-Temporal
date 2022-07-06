181868347462c1cceeb7533ef422e49e02ece0eb
juanvalenciaceiba <juan.valencia@ceiba.com.co>
2022-07-06 13:57:01 -0500

    servicios de usuarios completados

--- src/main/java/co/edu/uco/arquisw/dominio/usuario/puerto/usuario/comando/UsuarioRepositorioComando.java
+++ src/main/java/co/edu/uco/arquisw/dominio/usuario/puerto/usuario/comando/UsuarioRepositorioComando.java
@@ -2,7 +2,9 @@ package co.edu.uco.arquisw.dominio.usuario.puerto.usuario.comando;
 
 public interface UsuarioRepositorioComando
 {
-    Long guardar(String correo, String clave);
-    Long actualizar(String correo, String clave, Long id);
+    void guardar(String correo, String clave);
+    void actualizar(String correo, String clave, Long id);
     void eliminar(Long id);
+    boolean existeCorreo(String correo);
+
 }
